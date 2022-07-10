10f6e56cfeba8da81e7e1b6def977833d4f8d4f7
Mike <mike.s3crypto@gmail.com>
2022-07-10 11:39:12 +0100

    finalUpdate
    FInished what I could with task without being able to compile properly or connect to network

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,23 +1,48 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >0.4.22 <=0.8.15;
+pragma solidity >0.4.22 <0.8.16;
 
-import {IPoolAddressesProvider} from "https://github.com/aave/aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
-import {IPool} from "https://github.com/aave/aave-v3-core/contracts/interfaces/IPool.sol";
-import {ERC20} from "../node_modules/OpenZeppelin/contracts/token/ERC20/ERC20.sol";
+//import {IPoolAddressesProvider} from "https://github.com/aave/aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
+//import {IPool} from "https://github.com/aave/aave-v3-core/contracts/interfaces/IPool.sol";
+//import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import {Contract} from "@web3-eth-contract";
 
 contract Main {
-    address key;
+    function main(string _key) external returns (int256, int256) {
+        //IPoolAddressesProvider public provider = new provider();
+        //IPool public pool = IPool(provider.getPool());
+        string key = _key;
 
-    function main(address _key) external {
-        key = _key;
-        getBalance();
+        int256 userBalance = getUserBalance();
+        if (userBalance > 0) {
+            deposit(pool, userBalance);
+            borrow(pool);
+        }
+        return (getCollateralBalance(key, pool), getDebtBalance(key, pool));
     }
 
-    function getBalance() returns (uint256) {
-        address tokenContract = new web3.eth.Contract(ERC20).at(
-            
+    function getUserBalance() internal returns (int256) {
+        Contract tokenContract = new Contract(
+            web3
+            .eth
+            .Contract().at() /*USDC-ABI*/ /*USDC-Address*/
         );
-        int256 balance = tokenContract.balanceOf(0x3E937B4881CBd500d05EeDAB7BA203f2b7B3f74f);
-        return balance / Math.pow(10, decimal);
+        int256 balance = tokenContract.balanceOf(); /*User-Address*/
+        return (balance / Math.pow(10, decimal));
+    }
+
+    function deposit(IPool pool, int256 amount) internal {
+        pool.deposit({/*User-Address, USDC-Address, amount*/});
+    }
+
+    function borrow(IPool pool) internal {
+        //Borrowing Amount Calculation
+        pool.borrow({/*User-Address, USDC-Address, amount, 'Stable';*/});
+    }
+
+    function getCollateralBalance(string _key, IPool) internal returns (int256 collateral) {
+        return pool.getUserAccountData(_key).totalCollateralBase;
+    }
+
+    function getDebtBalance(string _key, IPool) internal returns (int256 debt) {
+        return pool.getUserAccountData(_key).totalDebtBase;
     }
 }
